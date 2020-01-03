9c891f5c83e498f93f5b73d9e7ae4dffa3c976f0
Ivan Chyr - Codete <ivan.chyr@codete.com>
2020-01-03 12:40:54 +0100

    added more logging to json

--- json.go
+++ json.go
@@ -1,37 +1,82 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/davecgh/go-spew/spew"
+	"log"
 )
 
 type Address struct {
-	street  string `json:"street,omitempty"`
-	city    string `json:"city,omitempty"`
-	country string `json:"country,omitempty"`
+	street  string `json:"street"`
+	city    string `json:"city"`
+	country string `json:"country"`
 }
 
 type User struct {
-	username  string   `json:"username,omitempty"`
-	height    int      `json:"height,omitempty"`
-	address   Address  `json:"address,omitempty"`
-	interests []string `json:"interests,omitempty"`
+	username  string   `json:"username"`
+	height    int      `json:"height"`
+	address   Address  `json:"address"`
+	interests []string `json:"interests"`
 }
 
 func main() {
 	address := Address{
-		street: "Main 1b/23",
-		city: "Big",
+		street:  "Main 1b/23",
+		city:    "Big",
 		country: "Important",
 	}
 	user := User{
 		username: "Martin",
-		height: 171,
-		address: address,
+		height:   171,
+		address:  address,
 		interests: []string{
 			"singin",
 			"dancing",
 		},
 	}
 
+	log.Println("Initial user object")
+	log.Println("")
 	spew.Dump(&user)
+	log.Println("")
+
+	// marshall json
+	data, err := json.Marshal(&user)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	log.Println("Marshalled byte array")
+	log.Println("")
+	log.Println(data)
+	log.Println("")
+
+	// unmarshall json into interface{}
+	var object interface{}
+	err = json.Unmarshal(data, &object)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	log.Println("UnMarshalled to interface{} user object")
+	log.Println("")
+	spew.Dump(object)
+	log.Println("")
+
+	log.Println("--------------------")
+	data2 := []byte(`{"name":"Marshal", "id": 13, "interests":["fire", "firefighter"], "address":{"city":"Adventure Bay"}}`)
+	log.Println("Marshalled byte array")
+	log.Println("")
+	log.Println(data2)
+	log.Println("")
+
+	err = json.Unmarshal(data2, &object)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	log.Println("UnMarshalled to interface{} user object")
+	log.Println("")
+	spew.Dump(object)
+	log.Println("")
 }
